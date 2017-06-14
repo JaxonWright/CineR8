9982ded9dae327b8b7d4aed8ef1e6e894328c5b8
JaxonWright <jaxonhwright@gmail.com>
2017-06-14 18:55:43 -0400

    base theme laid out. Basic routing works

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('movie');
+  this.route('login');
+  this.route('user');
+  this.route('show');
 });
 
 export default Router;
