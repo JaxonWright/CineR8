1b0e95d3879196ae8c1810693f9aa38c4628747f
JaxonWright <jaxonhwright@gmail.com>
2017-06-21 16:07:12 -0400

    Index page shows basic info for Most Popular and Highest Rated Movies.

---
+++ app/routes/index.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    model() {
+        return Ember.RSVP.hash({
+            mostPopular: Ember.$.getJSON("https://api.themoviedb.org/3/discover/movie?api_key=0910db5745f86638474ffefa5d3ba687&sort_by=popularity.desc"),
+            highestRated: Ember.$.getJSON("https://api.themoviedb.org/3/discover/movie?api_key=0910db5745f86638474ffefa5d3ba687&sort_by=vote_average.desc&vote_count.gte=1000")
+        });
+    }
+});
