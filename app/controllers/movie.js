428e066d514a5b4115a75921df9b1a77585da966
JaxonWright <jaxonhwright@gmail.com>
2017-06-27 19:57:03 -0400

    User rating works!

--- app/controllers/movie.js
+++ app/controllers/movie.js
@@ -2,7 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     actions: {
-        updateRating: function() {
+        updateRating(params) {
+             const { item: model, rating } = params;
+
+             let fanoutObject = {};
+             fanoutObject['users/' + this.get('session.currentUser.uid') + '/ratings/' + model.movie.id] = rating;
+             this.get('firebaseUtil').update(fanoutObject).then(() => {
+
+             }).catch(error => {
+                alert(error);
+             });
             
         }
     }
--- app/routes/movie.js
+++ app/routes/movie.js
@@ -3,7 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model(params) {
         return Ember.RSVP.hash({
-            movie: Ember.$.getJSON("https://api.themoviedb.org/3/movie/" + params.movie_id+ "?api_key=0910db5745f86638474ffefa5d3ba687")
+            movie: Ember.$.getJSON("https://api.themoviedb.org/3/movie/" + params.movie_id+ "?api_key=0910db5745f86638474ffefa5d3ba687"),
+            userRating: this.get('firebaseUtil').findRecord(params.movie_id, 'users/8V63eXOsjJhe9oAscvaLw1avxFH3/ratings/' + params.movie_id).catch(error => {
+                //error
+                alert(error);
+            })
         });
     }
 });
