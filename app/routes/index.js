cee02e1a97736e1f4323e0be2e7c155e75ff3dd0
JaxonWright <jaxonhwright@gmail.com>
2017-06-29 14:59:59 -0400

    modify the way user rating looks on movie page

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
     model() {
         return Ember.RSVP.hash({
             popularNow: Ember.$.getJSON("https://api.themoviedb.org/3/discover/movie?api_key=0910db5745f86638474ffefa5d3ba687&sort_by=popularity.desc"),
-            highestRated: Ember.$.getJSON("https://api.themoviedb.org/3/discover/movie?api_key=0910db5745f86638474ffefa5d3ba687&sort_by=vote_average.desc&vote_count.gte=1000")
+            highestRated: Ember.$.getJSON("https://api.themoviedb.org/3/discover/movie?api_key=0910db5745f86638474ffefa5d3ba687&sort_by=vote_average.desc&vote_count.gte=2000")
         });
     }
 });
