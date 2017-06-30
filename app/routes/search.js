598218073318cbb9e4a80f75c4d569aef31e9e8a
Ricardo Mendes <rokusu@gmail.com>
2017-06-30 09:09:15 +0100

    Update search.js

--- app/routes/search.js
+++ app/routes/search.js
@@ -5,7 +5,8 @@ export default Ember.Route.extend({
         value: {
              refreshModel: true
         }
-  },
+    },
+  
     model({ value }) {
         let query = value;
         let search = Ember.$.getJSON(`https://api.themoviedb.org/3/search/movie?query=${value}&api_key=0910db5745f86638474ffefa5d3ba687`);
