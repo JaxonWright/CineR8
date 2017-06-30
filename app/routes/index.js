e9b43748181662d6445cc4eb972b5bde792e5799
Ricardo Mendes <rokusu@gmail.com>
2017-06-30 09:15:42 +0100

    Update index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
+        let popularNow = Ember.$.getJSON("https://api.themoviedb.org/3/discover/movie?api_key=0910db5745f86638474ffefa5d3ba687&sort_by=popularity.desc");
+        let highestRated =  Ember.$.getJSON("https://api.themoviedb.org/3/discover/movie?api_key=0910db5745f86638474ffefa5d3ba687&sort_by=vote_average.desc&vote_count.gte=2000");
+
         return Ember.RSVP.hash({
-            popularNow: Ember.$.getJSON("https://api.themoviedb.org/3/discover/movie?api_key=0910db5745f86638474ffefa5d3ba687&sort_by=popularity.desc"),
-            highestRated: Ember.$.getJSON("https://api.themoviedb.org/3/discover/movie?api_key=0910db5745f86638474ffefa5d3ba687&sort_by=vote_average.desc&vote_count.gte=2000")
+            popularNow,
+            highestRated
         });
     }
 });
