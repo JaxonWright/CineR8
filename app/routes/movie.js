9df30bc06c0d299cf7692b8a7996d7cc8fa75d47
JaxonWright <jaxonhwright@gmail.com>
2017-06-29 21:02:50 -0400

    Grab cast and crew on movie page. Not doing anything with them yet.

--- app/routes/movie.js
+++ app/routes/movie.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model(params) {
         return Ember.RSVP.hash({
-            movie: Ember.$.getJSON("https://api.themoviedb.org/3/movie/" + params.movie_id+ "?api_key=0910db5745f86638474ffefa5d3ba687&append_to_response=videos"),
+            movie: Ember.$.getJSON("https://api.themoviedb.org/3/movie/" + params.movie_id+ "?api_key=0910db5745f86638474ffefa5d3ba687&append_to_response=videos,credits"),
             userRating: this.get('firebaseUtil').findRecord(params.movie_id, 'users/' + this.get('session.currentUser.uid') + '/ratings/' + params.movie_id).catch(error => {
                 //error
                 alert(error);
