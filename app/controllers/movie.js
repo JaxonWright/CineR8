ed8bd4644cdc1881c3fc134e39dfef831a5eb080
JaxonWright <jaxonhwright@gmail.com>
2017-06-30 15:04:40 -0400

    Fix typos in movie controller

--- app/controllers/movie.js
+++ app/controllers/movie.js
@@ -2,13 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     actions: {
-        updateRating({ item: { model: { movie } }, rating }) {
+        updateRating({ item: model, rating }) {
              let fanoutObject = {};
              let userId = this.get('session.currentUser.uid');
           
-             fanoutObject[`users/${'userId'}/ratings/${'movie.id'}/value`] = rating;
-             fanoutObject[`users/${'userId'}/ratings/${'movie.id'}/posterPath`] = movie.poster_path;
-             fanoutObject[`users/${'userId'}/ratings/${'movie.id'}/movieTitle`] = movie.title;
+             fanoutObject[`users/${userId}/ratings/${model.movie.id}/value`] = rating;
+             fanoutObject[`users/${userId}/ratings/${model.movie.id}/posterPath`] = model.movie.poster_path;
+             fanoutObject[`users/${userId}/ratings/${model.movie.id}/movieTitle`] = model.movie.title;
 
              this.get('firebaseUtil').update(fanoutObject).then(() => {
 
